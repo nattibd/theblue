17b8d0ab680bca8a16809791b17437b7d5ac0b7f
Natyra DeGraffenreid <natyraaa123@gmail.com>
2022-12-23 00:44:59 +0000

    push variables

--- newfile.py
+++ newfile.py
@@ -1,3 +1,24 @@
-"""
-Your module description
-"""
+x = 7
+y = "Joni"
+print(x)
+print(y)
+
+
+x, y, z = "Apple", "Mango", "Banana"
+print(x)
+print(y)
+print(z)
+
+
+x = 3
+y = 7
+print(x + y)
+
+
+def myfunc():
+  global x
+  x = "sweet"
+
+myfunc()
+
+print("Mango is " + x)
